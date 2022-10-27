b6b172ae28214b4cb1af1a0c52be229a6ee4a7c1
Kallen <kallenbierly@gmail.com>
2022-10-27 13:56:40 -0400

    running mostly as expected. Need to tweak a few things

--- src/HangmanGameRun.java
+++ src/HangmanGameRun.java
@@ -1,5 +1,4 @@
-import javax.swing.text.AttributeSet;
-import java.sql.SQLOutput;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,14 +9,14 @@ public class HangmanGameRun {
 
 
     public static void main(String[] args) {
-            List<String> dictionary = new ArrayList<>();
-            dictionary.add("Tiger");
-            dictionary.add("meadow");
-            dictionary.add("alpaca");
-            dictionary.add("butterfly");
+        List<String> dictionary = new ArrayList<>();
+        dictionary.add("Tiger");
+        dictionary.add("meadow");
+        dictionary.add("alpaca");
+        dictionary.add("butterfly");
 
 
-        int randomSelection = (int)(Math.random()*(dictionary.size() - 1));
+        int randomSelection = (int) (Math.random() * (dictionary.size() - 1));
         String selection = dictionary.get(randomSelection);
         Scanner input = new Scanner(System.in);
         System.out.println("Want to play Hangman? You get 6 tries to guess the word I'm thinking of!");
@@ -26,22 +25,65 @@ public class HangmanGameRun {
         System.out.println();
         System.out.println("My word has " + selection.length() + " letters.");
         System.out.println();
-        System.out.println("(Enter a letter to guess):");
-        String userInput = input.nextLine();
 
-        int numberOfTimesLetterAppears = 0;
+        int numberOfLives = 6;
+        List<String> lettersOfGuess = new ArrayList<>();
+
+
         for (int i = 0; i < selection.length(); i++) {
-            if (selection.charAt(i) == userInput.charAt(0)) {
-                numberOfTimesLetterAppears++;
+            lettersOfGuess.add("*");
+        }
+
+
+
+        while (numberOfLives > 0) {
+            System.out.println("(Enter a letter to guess):");
+            String userInput = input.nextLine().toLowerCase();
+            char guess = userInput.charAt(0);
+
+            int numberOfTimesLetterAppears = 0;
+            for (int i = 0; i < selection.length(); i++) {
+                if (selection.charAt(i) == guess) {
+                    numberOfTimesLetterAppears++;
+                }
             }
-        }
 
-        if (selection.toLowerCase().contains(userInput.toLowerCase())) {
-            System.out.println("My word contains " + numberOfTimesLetterAppears + " " + userInput.toUpperCase() + "(s).");
+            if (selection.toLowerCase().contains(userInput.toLowerCase())) {
+                System.out.println("My word contains " + numberOfTimesLetterAppears + " " + userInput.toUpperCase() + "(s).");
+
+
+                for (int i = 0; i < lettersOfGuess.size(); i++) {
+                    if (selection.charAt(i) == guess) {
+                        lettersOfGuess.remove(i);
+                        lettersOfGuess.add(i, userInput);
+                    }
+
+                }
+                String output = String.join("", lettersOfGuess);
+                System.out.println(output);
+
+                if (output.equals(selection)) {
+                    break;
+                }
+
+
+
+
+            } else {
+                System.out.println("My word doesn't contain that letter! You lose a life!");
+                numberOfLives--;
+                System.out.println("You have " + numberOfLives + " lives left!");
+            }
+            System.out.println("-----------------------------------");
+            System.out.println();
+
+
+        }
+        System.out.println("-----------------------------------");
+        if (numberOfLives == 0) {
+            System.out.println("You lose! I win!");
         } else {
-            System.out.println("My word doesn't contain that letter! You lose a life!");
+            System.out.println("You win!");
         }
-
-
     }
 }
